febc3f3090a457cb470e4a8b9cbd314f23dac464
Baronsec-code <enthurocks8@gmail.com>
2025-12-31 07:41:33 -0500

    script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
-function sayHello() {
-  document.getElementById("output").innerText =
-    "JavaScript is working!";
+function sendText() {
+  const text = document.getElementById("userInput").value;
+
+  fetch("https://cristopher-subcordiform-lacey.ngrok-free.dev/api/echo", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ text: text })
+  })
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById("output").innerText = data.reply;
+    })
+    .catch(error => {
+      document.getElementById("output").innerText =
+        "Error talking to Python";
+      console.error(error);
+    });
 }
