4e4ec86507108b32f1833fae30e1f84bfee34c83
Baronsec-code <enthurocks8@gmail.com>
2025-12-31 08:33:48 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function sendAI() {
   const text = document.getElementById("userInput").value;
   const encoded = encodeURIComponent(text);
 
-  fetch(`https://cristopher-subcordiform-lacey.ngrok-free.dev/api/ai?text=${encoded}`)
+  fetch(`https://cristopher-subcordiform-lacey.ngrok-free.dev/api/ai?text=hello`)
     .then(response => response.json())
     .then(data => {
       document.getElementById("output").innerText = data.reply;
