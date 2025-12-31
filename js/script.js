d4cafbb466d7a66085d1b5041034043050a9a6d1
Baronsec-code <enthurocks8@gmail.com>
2025-12-31 08:40:59 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
-function sendAI() {
+function sendText() {
   const text = document.getElementById("userInput").value;
-  const encoded = encodeURIComponent(text);
 
-  fetch(`https://cristopher-subcordiform-lacey.ngrok-free.dev/api/ai?text=${encoded}`)
-  .then(res => res.json())
+  fetch("https://cristopher-subcordiform-lacey.ngrok-free.dev/api/chat", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ text: text })
+  })
+    .then(response => response.json())
     .then(data => {
       document.getElementById("output").innerText = data.reply;
     })
     .catch(error => {
+      document.getElementById("output").innerText =
+        "AI server error";
       console.error(error);
-      document.getElementById("output").innerText = "AI error";
     });
 }
